52121ac3ce6cf20663431636ffd9db7c1a745041
Kevin Wijaya <kevin.tanuhardi@gmail.com>
2018-12-06 17:49:37 +0700

    skeleton

---
+++ vue.js
@@ -0,0 +1,11 @@
+const app = new Vue ({
+    el: "#app",
+    data: {
+        totalFeed: [
+            {
+                userName: "kevinTanuhardi",
+                imgUrl: ""
+            }
+        ],
+    }
+})
