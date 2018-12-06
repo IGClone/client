0be5f46370e9b80ed05a4efe4a1231a93f0c569f
Kevin Wijaya <kevin.tanuhardi@gmail.com>
2018-12-06 20:41:08 +0700

    layout done, userList not in component yet

--- vue.js
+++ vue.js
@@ -4,8 +4,31 @@ const app = new Vue ({
         totalFeed: [
             {
                 userName: "kevinTanuhardi",
-                imgUrl: ""
+                imgUrl: "http://cdn2.tstatic.net/makassar/foto/bank/images/dono-kasiono-indro_20180508_172559.jpg"
+            },
+            {
+                userName: "andreHok",
+                imgUrl: "https://9to5mac.com/wp-content/uploads/sites/6/2018/08/colorware.jpg?quality=82&strip=all&w=1500"
             }
         ],
+        usersList : [
+            {
+                userName: "kevinTanuhardi"
+            },
+            {
+                userName: "imanuVi"
+            },
+            {
+                userName: "andreHok"
+            },
+            {
+                userName: "dhimasHary"
+            }
+        ]
+    },
+    methods: {
+        shout(payload){
+            alert(payload)
+        }
     }
 })
