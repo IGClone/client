48e1dd7918042c4411d1f04f2df326fa420dd8af
Kevin Wijaya <kevin.tanuhardi@gmail.com>
2018-12-07 09:36:00 +0700

    layout on each component

--- components/feed.js
+++ components/feed.js
@@ -1,5 +1,5 @@
 Vue.component (`feed`,{
-    template: `<div class="col-md-8" id="feed">
+    template: `<div class="col-md-6" id="feed">
             <div v-for="feed in totalfeed">
                 <div class="card pb-0">
                     <div class="card-body pb-0"><h5 class="font-weight-light">{{feed.userName}}</h5></div>
@@ -18,7 +18,7 @@ Vue.component (`feed`,{
 
                                 <!-- facebook button -->
                                 <!-- Your share button code -->
-                                <div class="fb-share-button" data-href="https://www.instagram.com/developer/" data-layout="button_count" data-size="large" data-mobile-iframe="false"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.instagram.com%2Fdeveloper%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+                                <div class="fb-share-button mb-3" data-href="https://www.instagram.com/developer/" data-layout="button_count" data-size="large" data-mobile-iframe="false"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.instagram.com%2Fdeveloper%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
                         </div>
                     </div>
                 <div class="pt-5"></div>
@@ -29,7 +29,7 @@ Vue.component (`feed`,{
         props: ["totalfeed"],
         methods: {
             cobaAja(){
-                this.$emit("apaja", "woeee liat dong")
+                this.$emit("apa-aja", "woeee liat dong")
             }
         }
 })
